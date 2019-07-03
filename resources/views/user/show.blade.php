73b7466b0064e09793eb0073ab2a317eff2bbae3
bynl-44 <xyxcnnh@icloud.com>
2019-07-03 15:31:44 +0800

    显示头像

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -6,8 +6,7 @@
   <div class="row">
     <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
       <div class="card">
-        <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600"
-             alt="{{ $user->name }}" class="card-img-top">
+        <img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="card-img-top">
         <div class="card-body">
           <h5><strong>个人简介</strong></h5>
           <p>{{ $user->introduction }}</p>
@@ -21,12 +20,13 @@
       <div class="card">
         <div class="card-body">
           <h1 class="mb-0">
-            {{ $user->name }} <small>{{ $user->email }}</small>
+            {{ $user->name }}
+            <small>{{ $user->email }}</small>
           </h1>
         </div>
       </div>
       <hr>
-{{--      用户发布的内容--}}
+      {{--      用户发布的内容--}}
       <div class="card">
         <div class="card-body">
           暂无数据...
