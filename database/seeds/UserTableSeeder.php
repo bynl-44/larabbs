3b162bac296d3601ad37f793a0f13694f0b19958
bynl-44 <xyxcnnh@icloud.com>
2019-07-06 18:13:39 +0800

    支持队列的邮件通知

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -44,7 +44,7 @@ class UserTableSeeder extends Seeder
         // 单独处理第一个用户的数据
         $user = User::find(1);
         $user->name = 'admin';
-        $user->email = 'admin@123.com';
+        $user->email = 'zizizig@163.com';
         $user->password = bcrypt('admin');
         $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
