f57e6cc8687f10e7878f2cfdc9aed3c1c6b9acc4
bynl-44 <xyxcnnh@icloud.com>
2019-07-27 18:37:35 +0800

    notifications index api

---
+++ app/Transformers/NotificationTransformer.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Transformers;
+
+
+use Illuminate\Notifications\DatabaseNotification;
+use League\Fractal\TransformerAbstract;
+
+class NotificationTransformer extends TransformerAbstract
+{
+    public function transform(DatabaseNotification $notification)
+    {
+        return [
+            'id' => $notification->id,
+            'type' => $notification->type,
+            'data' => $notification->data,
+            'read_at' => $notification->read_at ? $notification->read_at->toDataTimeString() : null,
+            'created_at' => (string)$notification->created_at,
+            'updated_at' => (string)$notification->updated_at,
+        ];
+    }
+}
