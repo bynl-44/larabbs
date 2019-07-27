03aa19092b2d28d83cd894a6f922ab8c24495dae
bynl-44 <xyxcnnh@icloud.com>
2019-07-27 18:02:54 +0800

    replies destroy api

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Requests\Api\ReplyRequest;
 use App\Models\Reply;
 use App\Models\Topic;
+use App\Models\User;
 use App\Transformers\ReplyTransformer;
+use Dingo\Api\Http\Response;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
 
 class RepliesController extends Controller
@@ -18,5 +21,23 @@ class RepliesController extends Controller
         $reply->save();
 
         return $this->response->item($reply, new ReplyTransformer())->setStatusCode(201);
+    }
+
+    /**
+     * @param Topic $topic
+     * @param Reply $reply
+     * @return Response|void
+     * @throws AuthorizationException
+     */
+    public function destroy(Topic $topic, Reply $reply)
+    {
+        if ($reply->topic_id != $topic->id) {
+            return $this->response->errorBadRequest();
+        }
+
+        $this->authorize('destroy', $reply);
+        $reply->delete();
+
+        return $this->response->noContent();
     }
 }
